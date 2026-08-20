c7750fe5b4b11774989bda64c70ccada26dfe3eb
Harshachalla-sudo <harshachallac@gmail.com>
2026-08-20 11:26:13 +0530

    Add files via upload

---
+++ Lab Programs/exp-1.R
@@ -0,0 +1,11 @@
+a = c(1,2,3,4,5,6)
+a
+print(paste("Type: ", class(a)))
+
+b = c("Gokul","Raghul","Dhanush")
+b
+print(paste("Type: ", class(b)))
+
+c1 = c(TRUE, FALSE, TRUE, FALSE)
+c1
+print(paste("Type: ", class(c1)))
